634ef94236772252568217e99fd4cb9f9177fef4
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-10-02 23:35:10 -0300

    ADD: home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:changapp/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,11 +6,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
     return Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          title: const Text("ChangApp"),
-          //leading: Image.asset(name),
-        ));
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Centra verticalmente
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // Centra horizontalmente
+          children: [
+            Image.asset('assets/changappLogo.png'),
+            const Text('Changas y trabajos pequeños en el hogar...y más!!'),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [Text('Soy el login')],
+      ),
+    );
+  }
+}
