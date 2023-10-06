4800f00c75d081d1990ba9b4d0a62c7d6a839802
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-10-05 22:26:06 -0300

    ADD: login button - text register

--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -14,31 +14,50 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Form(
         key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: const BorderSide(color: Colors.white))),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Password',
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: const BorderSide(color: Colors.white))),
-            )
-          ],
-        ));
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Image.asset('assets/changappLogo.png'),
+          const SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.white)),
+                filled: true,
+                fillColor: Colors.white70),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Password',
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.white)),
+                filled: true,
+                fillColor: Colors.white70),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber.shade600,
+                  shape: const StadiumBorder(
+                      side: BorderSide(color: Colors.black54))),
+              onPressed: () {},
+              child: const Text(
+                'Iniciar Sesión',
+                style: TextStyle(color: Colors.black45),
+              )),
+          const SizedBox(height: 35),
+          const Text('No estás registrado?',
+              style: TextStyle(color: Colors.black87))
+        ]));
   }
 }
